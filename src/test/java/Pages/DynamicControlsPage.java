d0b19775a87933239f59d605184c1605ac9d60c2
Arkad1y7 <110785083+Arkad1y7@users.noreply.github.com>
2025-01-18 13:10:18 +0300

    fix DynamicControlsPage

--- src/test/java/Pages/DynamicControlsPage.java
+++ src/test/java/Pages/DynamicControlsPage.java
@@ -18,26 +18,21 @@ public class DynamicControlsPage extends SeleniumBasePage{
     public void RemoveAdd() {
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(3));
 
-        try {
-            WebElement checkbox = driver.findElement(By.cssSelector("input[type='checkbox']"));
-            if (checkbox.isDisplayed()) {
-                driver.findElement(By.cssSelector("button[onclick='swapCheckbox()']")).click();
+        WebElement checkbox = driver.findElement(By.cssSelector("input[type='checkbox']"));
+        if (checkbox.isDisplayed()) {
+            driver.findElement(By.cssSelector("button[onclick='swapCheckbox()']")).click();
 
-                try {
-                    wait.until(ExpectedConditions.invisibilityOf(checkbox)); //wait until checkbox to be invisible
-                } catch (TimeoutException e) {
-                    System.out.println("FAIL: Checkbox did not disappear within the expected time");
-                }
-
-                try {
-                    checkbox = wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector("input[type='checkbox']"))); //redefining the variable because it is no longer in the DOM
-                    assertFalse(checkbox.isEnabled(), "FAIL: Checkbox is still on the page");
-                } catch (TimeoutException e) {
-                    System.out.println("PASS: Checkbox after click button \"Remove\" is no longer present on the page after the action");
-                }
+            try {
+                wait.until(ExpectedConditions.invisibilityOf(checkbox)); //wait until checkbox to be invisible
+            } catch (TimeoutException e) {
+                System.out.println("FAIL: Checkbox did not disappear within the expected time");
             }
-        } catch (TimeoutException e) {
-            System.out.println("FAIL: Checkbox was not found within the expected time");
+
+            try {
+                checkbox = driver.findElement(By.cssSelector("input[type='checkbox']"));
+            } catch (Exception e) {
+                System.out.println("PASS: Checkbox after click button \"Remove\" is no longer present on the page after the action");
+            }
         }
     }
 }
